ac03e454300141b851da6fb93c7c0340b79d56a5
Le Minh Tri <ansidev@gmail.com>
2021-01-20 04:56:03 +0700

    style(config): change indentation size to 2 spaces

--- webpack.config.js
+++ webpack.config.js
@@ -9,68 +9,68 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
 
-    mode: isProduction ? 'production' : 'development',
+  mode: isProduction ? 'production' : 'development',
 
-    entry: './src/vueDatePick.vue',
+  entry: './src/vueDatePick.vue',
 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'vueDatePick.js',
-        library: 'VueDatePick',
-        libraryExport: 'default',
-        libraryTarget: 'umd',
-        globalObject: 'typeof self !== \'undefined\' ? self : this'
-    },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'vueDatePick.js',
+    library: 'VueDatePick',
+    libraryExport: 'default',
+    libraryTarget: 'umd',
+    globalObject: 'typeof self !== \'undefined\' ? self : this'
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    isProduction ? MiniCssExtractPlugin.loader : 'vue-style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                include: [
-                    path.join(__dirname, 'src')
-                ]
-            }
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'vue-style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
         ]
-    },
-
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: false
-            }),
-            new OptimizeCSSAssetsPlugin({})
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [
+          path.join(__dirname, 'src')
         ]
-    },
+      }
+    ]
+  },
 
-    resolve: {
-        extensions: ['*', '.js', '.vue', '.json']
-    },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
 
-    plugins: [
-        new VueLoaderPlugin(),
-        new ESLintPlugin({
-            extensions: ['js', 'vue'],
-            files: [
-                path.join(__dirname, 'src'),
-                path.join(__dirname, 'spec')
-            ]
-        }),
-        isProduction && new MiniCssExtractPlugin({filename: 'vueDatePick.css'})
-    ].filter(i => i)
+  resolve: {
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+
+  plugins: [
+    new VueLoaderPlugin(),
+    new ESLintPlugin({
+      extensions: ['js', 'vue'],
+      files: [
+        path.join(__dirname, 'src'),
+        path.join(__dirname, 'spec')
+      ]
+    }),
+    isProduction && new MiniCssExtractPlugin({filename: 'vueDatePick.css'})
+  ].filter(i => i)
 };
